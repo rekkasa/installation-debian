f91f2889d83dc3be958c29b38f86c7df77ef7ec1
rekkasa <a.rekkas@erasmusmc.nl>
2021-11-28 15:21:03 +0100

    switch to qtile

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,21 +4,24 @@ NAME=${1?2Error: no user given}
 USERHOME=/home/$NAME
 
 # Replace sources.list
-sudo su
-[[ -f /etc/apt/sources.list ]] && { mv /etc/apt/sources.list /etc/apt/sources.list.bak; cp $USERHOME/installation-debian/files/sources.list /etc/apt/sources.list; }
-[[ -f /etc/apt/preferences ]] && { cp $USERHOME/installation-debian/files/preferences /etc/apt/preferences; }
+[[ -f /etc/apt/sources.list ]] && { sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak; sudo cp $USERHOME/installation-debian/files/sources.list /etc/apt/sources.list; }
+[[ -f /etc/apt/preferences ]] && { sudo cp $USERHOME/installation-debian/files/preferences /etc/apt/preferences; }
 
-apt update && apt upgrade -yy
-apt install xorg xinit build-essential libx11-dev intel-microcode \
+sudo apt update && apt upgrade -yy
+sudo apt install xorg xserver-xorg xinit build-essential libx11-dev intel-microcode \
+	python3 python3-pip python-gobject python-dbus libpangocairo-1.0.0 \
 	materia-gtk-theme numix-icon-theme lxappearance \
 	libxft-dev libxinerama-dev feh picom thunar qpdfview \
 	network-manager xbacklight curl cmake pkg-config \
 	libfreetype6-dev libfontconfig1-dev alsa-utils \
-	libxcb-xfixes0-dev libxkbcommon-dev python3 firefox ufw wget\
+	libxcb-xfixes0-dev libxkbcommon-dev firefox ufw wget\
 	r-cran-curl r-cran-openssl r-cran-xml2\
 	libcurl4-openssl-dev libxml2-dev libssl-dev \
 	fonts-dejavu fonts-inconsolata \
 	flameshot apt-listbugs psmisc r-base -yy
+
+pip3 install xcffib
+pip3 install --no-cache-dir cairocffi
 
 curl -fsSL https://starship.rs/install.sh | bash
 exit
@@ -36,22 +39,26 @@ cd $USERHOME/src && git clone https://git.suckless.org/dwm
 cd $USERHOME/src && git clone https://git.suckless.org/dmenu
 cd $USERHOME/src && git clone https://github.com/dudik/herbe.git
 cd $USERHOME/src && git clone https://git.suckless.org/slstatus
+cd $USERHOME/src && git clone https://github.com/qtile/qtile.git
 # cd $USERHOME && git clone https://gitlab.com/dwt1/wallpapers.git
 # mv $USERHOME/wallpapers/ $USERHOME/Wallpapers/
 cd $USERHOME/git && git clone https://github.com/zsh-users/zsh-autosuggestions.git && \
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 # Fonts
-echo -e "\n\nInstsalling Nerd fonts\n"
-cd $USERHOME/src && git clone https://github.com/ryanoasis/nerd-fonts.git
-$USERHOME/src/nerd-fonts/install.sh
+# echo -e "\n\nInstsalling Nerd fonts\n"
+# cd $USERHOME/src && git clone https://github.com/ryanoasis/nerd-fonts.git
+# $USERHOME/src/nerd-fonts/install.sh
 
 # Setup xinit
 ln -s $USERHOME/configs/xinitrc $USERHOME/.xinitrc
 
 # Setup dwm
-ln -s $USERHOME/configs/dwm/config.h $USERHOME/src/dwm/
-cd $USERHOME/src/dwm && make
+# ln -s $USERHOME/configs/dwm/config.h $USERHOME/src/dwm/
+# cd $USERHOME/src/dwm && make
+
+# Setup qtile
+cd $USERHOME/src/qtile && pip3 install .
 
 # Setup dmenu
 cd $USERHOME/src/dmenu && make
